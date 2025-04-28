a3104fa88665c390b6e541c3993f7247d6c38005
senolburak <buraksenol00@outlook.com>
2025-04-28 14:50:54 +0300

    Creates classes and dependencies

---
+++ HastaneSimulasyonu.UI/Models/Bolum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HastaneSimulasyonu.UI.Models
+{
+    public class Bolum
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Aciklama { get; set; }
+        public List<Doktor> Doktorlar { get; set; }
+    }
+}
